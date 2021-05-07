2e6a6021238a36bf1ca0dbb4f2eee188ea383488
kaveencharuka <kaveencharuka@gmail.com>
2021-05-07 11:25:09 +0530

    main activity java

--- app/src/main/java/com/mad2021june/dianacosmetics/MainActivity.java
+++ app/src/main/java/com/mad2021june/dianacosmetics/MainActivity.java
@@ -1,14 +1,60 @@
 package com.mad2021june.dianacosmetics;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.mad2021june.dianacosmetics.Model.Users;
+import com.mad2021june.dianacosmetics.Prevalent.Prevalent;
+
+import io.paperdb.Paper;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button joinNowButton, loginButton;
+    private ProgressDialog loadingBar;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        joinNowButton = (Button) findViewById(R.id.main_join_now_btn);
+        loginButton = (Button) findViewById(R.id.main_login_btn);
+        loadingBar = new ProgressDialog(this);
+
+        Paper.init(this);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        joinNowButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
