c3c500326c56204bad57f0d15c858dfc55bea50b
VibhushaSingappuli <singappuli.dewan67@gmail.com>
2021-05-08 20:43:20 +0530

    Add Home Activity with Navigation Features

---
+++ app/src/main/java/com/mad2021june/dianacosmetics/HomeActivity.java
@@ -0,0 +1,128 @@
+package com.mad2021june.dianacosmetics;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Menu;
+import android.widget.TextView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.navigation.NavigationView;
+
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import io.paperdb.Paper;
+
+public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_home );
+
+        Paper.init(this);
+        Toolbar toolbar = findViewById( R.id.toolbar );
+        //Change1
+        toolbar.setTitle( "Home" );
+        setSupportActionBar( toolbar );
+
+        FloatingActionButton fab = findViewById( R.id.fab );
+        fab.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make( view, "Replace with your own action", Snackbar.LENGTH_LONG )
+                        .setAction( "Action", null ).show();
+            }
+        } );
+
+        DrawerLayout drawer = findViewById( R.id.drawer_layout );
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = findViewById( R.id.nav_view );
+        navigationView.setNavigationItemSelectedListener(this);
+
+    }
+
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen( GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate( R.menu.home, menu );
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        int id = item.getItemId();
+
+        //if (id == R.id.action_settings) {
+        //  return true;
+        //}
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item)
+    {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_cart)
+        {
+
+        }
+        else if (id == R.id.nav_payment)
+        {
+
+        }
+        else if (id == R.id.nav_enquiry)
+        {
+
+        }
+        else if (id == R.id.nav_settings)
+        {
+            //Intent intent = new Intent(HomeActivity.this, SettingsActivity.class);
+            //startActivity(intent);
+        }
+        else if (id == R.id.nav_logout)
+        {
+            Paper.book().destroy();
+
+            Intent intent = new Intent(HomeActivity.this,MainActivity.class);
+            intent.addFlags( Intent.FLAG_ACTIVITY_NEW_TASK| Intent.FLAG_ACTIVITY_CLEAR_TASK );
+            startActivity( intent );
+            finish();
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+}
