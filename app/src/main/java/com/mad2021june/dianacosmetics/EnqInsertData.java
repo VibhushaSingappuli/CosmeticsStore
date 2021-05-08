f4c054b23435d8073deaaf8d7d520aca57256beb
virajA404 <abeywardana21@gmail.com>
2021-05-08 23:50:56 +0530

    Add activity_enq_insert_data.xml

---
+++ app/src/main/java/com/mad2021june/dianacosmetics/EnqInsertData.java
@@ -0,0 +1,14 @@
+package com.mad2021june.dianacosmetics;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EnqInsertData extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enq_insert_data);
+    }
+}
